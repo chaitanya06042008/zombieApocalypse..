04d25ba18f61559ac43b894de2c4887339368fb7
chaitanya06042008 <73021572+chaitanya06042008@users.noreply.github.com>
2020-11-17 16:08:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(700, 700);
+	rectMode(CENTER);
+	
+	box5 = createSprite( 360, 650, 200, 20)
+	box5.shapeColor= "red"
+	box1 = createSprite(250 ,610, 20, 100)
+	box1.shapeColor= "red"
+	box2 = createSprite(465 ,610, 20, 100)
+	box2.shapeColor= "red"
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	Box = Bodies.rectangle(360 , 630 , 200, 20 , { isStatic:true});
+	World.add(world, Box);
+	//Create a Ground
+	ground = Bodies.rectangle(350, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	Engine.update(engine)
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  //box5.x= Box.position.x 
+  //box5.y= Box.position.y 
+  drawSprites();
+  
+  keyPressed()
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+Matter.Body.setStatic(packageBody, false)
+	
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
+    
+  }
+}
+
+
+
